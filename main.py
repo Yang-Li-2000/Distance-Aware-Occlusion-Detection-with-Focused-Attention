847f0586ecb5fc006e465c6eba7ff58d53c05539
Alphaaaaabet <50163745+Alphaaaaabet@users.noreply.github.com>
2021-12-14 15:55:37 +0800

    Updated indents in main.py when printing magic numbers.

--- main.py
+++ main.py
@@ -152,11 +152,11 @@ def main(args):
     device = torch.device(args.device)
 
     print()
-    print("USE_SMALL_ANNOTATION_FILE:", USE_SMALL_ANNOTATION_FILE)
-    print("USE_OPTIMAL_TRANSPORT:", USE_OPTIMAL_TRANSPORT)
-    print("USE_DEPTH_DURING_TRAINING:", USE_DEPTH_DURING_TRAINING)
-    print("PREDICT_INTERSECTION_BOX:", PREDICT_INTERSECTION_BOX)
-    print("CASCADE:", CASCADE)
+    print("USE_SMALL_ANNOTATION_FILE:   ", USE_SMALL_ANNOTATION_FILE)
+    print("USE_OPTIMAL_TRANSPORT:       ", USE_OPTIMAL_TRANSPORT)
+    print("USE_DEPTH_DURING_TRAINING:   ", USE_DEPTH_DURING_TRAINING)
+    print("PREDICT_INTERSECTION_BOX:    ", PREDICT_INTERSECTION_BOX)
+    print("CASCADE:                     ", CASCADE)
     if CASCADE:
         print("dec_layers | dec_layers_distance | dec_layers_distance: ")
         print(args.dec_layers, "         |", args.dec_layers_distance, "                  |", args.dec_layers_distance)
